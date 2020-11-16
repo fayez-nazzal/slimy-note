8d27e0fddc8a86ff99ace1f41bd0a332b8088687
fayez-nazzal <soulldev@gmail.com>
2020-11-16 12:52:53 +0200

    updated tools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Redirect, Route } from 'react-router-dom'
-import { ReactComponent as Search } from './search.svg'
-import { Richmon } from 'react-richmon'
-import { styled } from 'react-richmon'
-import './styles.css'
-import NotebookItem from './NotebookItem'
-import ColFlex from './ColFlex'
-import StyledPlusIcon from './StyledText'
+import React, { useState } from "react";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import { ReactComponent as Search } from "./search.svg";
+import { Richmon } from "react-richmon";
+import { styled } from "react-richmon";
+import "./styles.css";
+import NotebookItem from "./NotebookItem";
+import ColFlex from "./ColFlex";
+import StyledPlusIcon from "./StyledText";
 
 const StyledInput = styled.input`
   padding: 6.5px;
@@ -18,136 +18,136 @@ const StyledInput = styled.input`
   }
   width: 134px;
   height: 16px;
-`
+`;
 
 const StyledSearchIcon = styled(Search)`
   width: 22px;
-`
+`;
 
 interface LabelProps {
-  padding?: string
-  fontSize?: string
-  margin?: string
+  padding?: string;
+  fontSize?: string;
+  margin?: string;
 }
 
 const StyledLabel = styled.label`
-  padding: ${(props: LabelProps) => (props.padding ? props.padding : 'unset')};
+  padding: ${(props: LabelProps) => (props.padding ? props.padding : "unset")};
   font-size: ${(props: LabelProps) =>
-    props.fontSize ? props.fontSize : 'unset'};
-  margin: ${(props: LabelProps) => (props.margin ? props.margin : 'unset')};
-`
+    props.fontSize ? props.fontSize : "unset"};
+  margin: ${(props: LabelProps) => (props.margin ? props.margin : "unset")};
+`;
 
 const App = () => {
   const [notebooks, setNotebooks] = useState([
     {
-      title: 'My Notebook',
+      title: "My Notebook",
       active: true,
-      color: '#eeeeee',
+      color: "#eeeeee",
       notes: [
         {
-          title: 'Welcome!',
-          color: '#a2d5f2',
-          content: '<div></div>'
-        }
-      ]
-    }
-  ])
-  const [searchText, setSearchText] = useState('')
-  const [activeNotebookIndex, setActiveNotebookIndex] = useState(0)
+          title: "Welcome!",
+          color: "#a2d5f2",
+          content: "<div></div>",
+        },
+      ],
+    },
+  ]);
+  const [searchText, setSearchText] = useState("");
+  const [activeNotebookIndex, setActiveNotebookIndex] = useState(0);
 
   const activateNotebook = (index: number) => {
-    const clonedNotebooks = [...notebooks]
+    const clonedNotebooks = [...notebooks];
     clonedNotebooks.forEach((notebook, notebookIndex) => {
-      notebook.active = notebookIndex === index
-    })
-    setNotebooks([...clonedNotebooks])
-    setActiveNotebookIndex(index)
-    if (!notebooks[index].notes) addNote(index)
-  }
+      notebook.active = notebookIndex === index;
+    });
+    setNotebooks([...clonedNotebooks]);
+    setActiveNotebookIndex(index);
+    if (!notebooks[index].notes) addNote(index);
+  };
 
   const changeNotebookTitle = (index: number, title: string) => {
-    const clonedNotebooks = [...notebooks]
-    clonedNotebooks[index].title = title
-    setNotebooks([...clonedNotebooks])
-  }
+    const clonedNotebooks = [...notebooks];
+    clonedNotebooks[index].title = title;
+    setNotebooks([...clonedNotebooks]);
+  };
 
   const changeNoteTitle = (
     notebookIndex: number,
     noteIndex: number,
     title: string
   ) => {
-    const clonedNotebooks = [...notebooks]
-    clonedNotebooks[notebookIndex].notes[noteIndex].title = title
-    setNotebooks([...clonedNotebooks])
-  }
+    const clonedNotebooks = [...notebooks];
+    clonedNotebooks[notebookIndex].notes[noteIndex].title = title;
+    setNotebooks([...clonedNotebooks]);
+  };
 
   const changeNotebookColor = (index: number, color: string) => {
-    const clonedNotebooks = [...notebooks]
-    clonedNotebooks[index].color = color
-    setNotebooks([...clonedNotebooks])
-  }
+    const clonedNotebooks = [...notebooks];
+    clonedNotebooks[index].color = color;
+    setNotebooks([...clonedNotebooks]);
+  };
 
   const changeNoteColor = (
     notebookIndex: number,
     noteIndex: number,
     color: string
   ) => {
-    const clonedNotebooks = [...notebooks]
-    clonedNotebooks[notebookIndex].notes[noteIndex].color = color
-    setNotebooks([...clonedNotebooks])
-  }
+    const clonedNotebooks = [...notebooks];
+    clonedNotebooks[notebookIndex].notes[noteIndex].color = color;
+    setNotebooks([...clonedNotebooks]);
+  };
 
   const addNote = (notebookIndex: number) => {
-    const clonedNotebooks = [...notebooks]
+    const clonedNotebooks = [...notebooks];
     clonedNotebooks[notebookIndex].notes.push({
-      title: 'new\u200b',
-      color: '#f4f3f3',
-      content: '<div></div>'
-    })
-    setNotebooks([...clonedNotebooks])
-  }
+      title: "new\u200b",
+      color: "#f4f3f3",
+      content: "<div></div>",
+    });
+    setNotebooks([...clonedNotebooks]);
+  };
 
   return (
     <BrowserRouter>
-      <ColFlex direction='row'>
-        <ColFlex style={{ width: '230px' }} direction='column'>
+      <ColFlex direction="row">
+        <ColFlex style={{ width: "230px" }} direction="column">
           <ColFlex
-            direction='row'
+            direction="row"
             style={{
-              justifyContent: 'space-around',
-              marginTop: '8px'
+              justifyContent: "space-around",
+              marginTop: "8px",
             }}
             alignItemsCenter
           >
-            <StyledLabel fontSize='20px'>Notebooks</StyledLabel>
+            <StyledLabel fontSize="20px">Notebooks</StyledLabel>
             <StyledPlusIcon
-              fontSize='29px'
+              fontSize="29px"
               onClick={() => {
                 setNotebooks([
                   ...notebooks,
                   {
-                    title: 'new\u200b',
+                    title: "new\u200b",
                     active: false,
-                    color: '#eeeeee',
+                    color: "#eeeeee",
                     notes: [
                       {
-                        title: 'new Note',
-                        content: '<div></div>',
-                        color: '#f4f3f3'
-                      }
-                    ]
-                  }
-                ])
+                        title: "new Note",
+                        content: "<div></div>",
+                        color: "#f4f3f3",
+                      },
+                    ],
+                  },
+                ]);
               }}
             >
               +
             </StyledPlusIcon>
           </ColFlex>
           <ColFlex
-            direction='row'
+            direction="row"
             style={{
-              justifyContent: 'space-around',
-              marginTop: '8px'
+              justifyContent: "space-around",
+              marginTop: "8px",
             }}
             alignItemsCenter
           >
@@ -160,7 +160,7 @@ const App = () => {
           <div>
             <hr />
           </div>
-          <ColFlex direction='column'>
+          <ColFlex direction="column">
             {notebooks
               .filter(
                 (notebook) =>
@@ -202,19 +202,33 @@ const App = () => {
               key={`route${noteIndex}`}
             >
               <Richmon
-                tools={['BIUS', 'fontSize', 'textColor', 'textHighlight', 'table']}
+                tools={[
+                  "BIUS",
+                  "thin-seperator",
+                  "sub",
+                  "sup",
+                  "thin-seperator",
+                  "fontSize",
+                  "textColor",
+                  "textHighlight",
+                  "textShadow",
+                  "thin-seperator",
+                  "table",
+                  "orderedList",
+                  "unorderedList",
+                ]}
                 content={note.content}
                 onChange={(newContent: string) => {
-                  const notebooksClone = [...notebooks]
+                  const notebooksClone = [...notebooks];
                   notebooksClone[activeNotebookIndex].notes[
                     noteIndex
-                  ].content = newContent
-                  setNotebooks([...notebooksClone])
+                  ].content = newContent;
+                  setNotebooks([...notebooksClone]);
                 }}
-                toolbarCss='border-bottom: 1px solid #cccccc'
-                width='100%'
-                height='100vh'
-                css='border-left: 1px solid #eeeeee;box-shadow: none;'
+                toolbarCss="border-bottom: 1px solid #cccccc"
+                width="100%"
+                height="100vh"
+                css="border-left: 1px solid #eeeeee;box-shadow: none;"
               />
             </Route>
           ))
@@ -228,7 +242,7 @@ const App = () => {
         </Route>
       </ColFlex>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
