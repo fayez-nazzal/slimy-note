e83a68de370260c981fd52c7ae8334c63ed8a363
fayez-nazzal <soulldev@gmail.com>
2020-11-17 15:51:24 +0200

    try to fix express error

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.use("/api", apiRouter);
 
 app.use("/auth", authRoutes);
 
-// app.use(express.static("./client/build"));
+app.use(express.static("./client/build"));
 
 app.set("view engine", "pug");
 
-// app.get("*", (req, res) => {
-//   // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 mongo.connect();
 
@@ -50,14 +50,14 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
